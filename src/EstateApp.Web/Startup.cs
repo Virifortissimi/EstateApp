b839fd26642bd027400d17905320e4022539795b
Virifortissimi <contacteghosa@gmail.com>
2020-06-02 03:02:34 +0100

    chore/effected migrations

--- src/EstateApp.Web/Startup.cs
+++ src/EstateApp.Web/Startup.cs
@@ -39,7 +39,7 @@ namespace EstateApp.Web
                     sqlServerOptions.MigrationsAssembly("EstateApp.Data");
                 }
             ));
-
+            
             services.AddControllersWithViews();
         }
 
