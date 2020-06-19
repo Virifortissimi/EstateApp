e17ea1b1181c0e586711e7d1111dc46203ad694b
Virifortissimi <contacteghosa@gmail.com>
2020-06-19 19:24:17 +0100

    feat/created register and login models

--- src/EstateApp.Web/Controllers/AccountsController.cs
+++ src/EstateApp.Web/Controllers/AccountsController.cs
@@ -1,13 +1,33 @@
+using System;
+using EstateApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EstateApp.Web.Controllers {
-    public class AccountsController : Controller {
-        public IActionResult Register () {
-            return View ();
+namespace EstateApp.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
         }
 
-        public IActionResult Login () {
-            return View ();
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid) return View();
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid) return View();
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/EstateApp.Web/Models/RegisterModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EstateApp.Web.Models
+{
+
+    public class RegisterModel
+    {
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Name")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
